e1b119e7fb6e85d179a4664ac612cf1140e08663
Akash Raturi <68784889+akashhraturi@users.noreply.github.com>
2025-05-07 15:10:57 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "akash33-s3-backend"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
