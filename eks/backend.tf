2f27129b9fe14d61de9c082c3c3229d01da97ec4
Akash Raturi <akashraturi33@gmail.com>
2025-05-07 15:19:51 +0530

    Comment out use_lockfile in backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -9,7 +9,7 @@ terraform {
     bucket         = "akash33-s3-backend"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    use_lockfile = "Lock-Files"
+    # use_lockfile = "Lock-Files"
     encrypt        = true
   }
 }
