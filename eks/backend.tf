cc7d5630decd96d6311fac74687dd72a53f48476
Akash Raturi <akashraturi33@gmail.com>
2025-05-07 15:16:06 +0530

    Fix backend.tf to use 'use_lockfile' instead of 'dynamodb_table' for state locking

--- eks/backend.tf
+++ eks/backend.tf
@@ -9,7 +9,7 @@ terraform {
     bucket         = "akash33-s3-backend"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    use_lockfile = "Lock-Files"
     encrypt        = true
   }
 }
